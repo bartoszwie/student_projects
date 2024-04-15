1d310c2bf9556306b3765609a3196d3527b97e48
bartoszwie <bwielgus_211i1@e.zseeim.edu.pl>
2024-04-15 09:19:53 +0000

    studentmachine send

---
+++ python/witaj_swiecie/main.py
@@ -0,0 +1,13 @@
+'''
+print ("witaj swiecie")
+x = 5
+y = 10
+
+if x < y:
+    print("x jest mniejsze niz y")
+elif y < x:
+    print("y jest mniejsze niz x")
+'''
+a = int(input("Podaj a"))
+b = int(input("Podaj b"))
+print (a+b)
