1bd3d6afc11d9fb000be33f34b3c71105be9bac8
bartoszwie <student@student.machine>
2024-09-23 11:06:45 +0000

    studentmachine send

---
+++ cpp/sortowanie/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int tablica[4] = (3,2,5,4);
+	for(int = o; i < 3; i++){
+	for(int j = 0; j < 3 - 1; j++){
+	if(tablica[j] > tablica[j + 1]{
+}
+
+
+
+}
+
+
+
+}
+	return 0;
+}
