12614b021a367fb5121f85173d4b9f7e1f2502fb
bartoszwie <bwielgus_211i1@e.zseeim.edu.pl>
2024-10-28 12:11:44 +0000

    studentmachine send

--- cpp/statki/main.cpp
+++ cpp/statki/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main(){
 	
-	int tab[3][3] = {0};
+	int tab[3][3]={0};
 	int x,y;
-	int gr, gc;
+	int a, b;
     cout <<"Wpisz wspolrzedne statku(x,y): ";
     cin >> x >> y;
     tab[x][y] = '0';
 
     for(int i = 0; i<3; i++){
     cout <<"Zgadnij wspolrzedne statku (wiersz i kolumna od 0 do 2): ";
-    cin >> gr >> gc;
+    cin >> a >> b;
 
-    if (tab[gr][gc] == '0'){
+    if (tab[a][b] == '0'){
     cout << "Trafiony!" <<endl;
     break;
     }else{
